8b2391a066dfd2d1a13f45480bc4d972899591a2
mohamed <mohamed.azeddagh1002@gmail.com>
2023-12-13 17:04:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoApp from './components/listing';
+
+function App(){
+  return(
+    <div style={{textAlign:'center'}}>
+      <TodoApp/>
+    </div>
+  );
+}
+export default App;
